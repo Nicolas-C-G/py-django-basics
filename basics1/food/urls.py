1869d64eba362b089a7b7310935c77ed16f4036d
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-05-08 22:07:27 -0400

    Updated datail and index

--- basics1/food/urls.py
+++ basics1/food/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'food'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('item/', views.item, name='item'),
+    path('<int:item_id>/', views.detail, name='detail'),
 ]
--- basics1/food/views.py
+++ basics1/food/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'item_list':item_list,
+    }
+    return render(request, 'food/index.html',context)
 
 def item(request):
-    return HttpRequest('<h1>This is an item</h1>')
+    return HttpResponse('<h1>This is an item</h1>')
+
+def detail(request,item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+    return render(request, 'food/detail.html',context);
