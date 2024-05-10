35ab4c005af40bcfa91f3dfb729ced843ff8dedf
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-05-10 16:00:38 -0400

    Login and logout: done

--- basics1/basics1/urls.py
+++ basics1/basics1/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('register/',user_views.register, name='register'),
-    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('login/', user_views.user_login,name='login'),
+    path('logout/', user_views.user_logout,name='logout'),
 ]
--- basics1/users/views.py
+++ basics1/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import logout, login, authenticate
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def register(request):
@@ -10,8 +13,27 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Welcome { username }, your account is created')
-            return redirect('food:home')
+            return redirect('login')
     else:
         form = RegisterForm()
     
     return render(request, 'users/register.html',{'form':form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('http://127.0.0.1:8000/food/')
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html', {})
