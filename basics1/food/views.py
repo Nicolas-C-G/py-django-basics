ffcce249a0b24bdd6a0f0f9cafd378588e8bcf7d
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-05-17 21:45:46 -0400

    Class based detail view sample

--- basics1/food/views.py
+++ basics1/food/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
 from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
@@ -14,6 +16,13 @@ def home(request):
     }
     return render(request, 'food/index.html',context)
 
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
+
 def item(request):
     return HttpResponse('<h1>This is an item</h1>')
 
@@ -23,6 +32,11 @@ def detail(request,item_id):
         'item':item,
     }
     return render(request, 'food/detail.html',context);
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+    
 
 def create_item(request):
     form = ItemForm(request.POST or None)
